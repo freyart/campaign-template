04c60325bb546b53a33fbe21118cf0f6d7afe795
Freyart <abnormalpoet@gmail.com>
2019-02-01 23:55:14 -0500

    Cleanup du site + modification des nouvelles

--- js/messcripts.js
+++ js/messcripts.js
@@ -28,10 +28,8 @@ function UpdateNews() {
             var sortedNews = data.news.sort(comp);
             $.each(sortedNews, function (i, item) {
                 if (i < 5) {
-                    if (i > 0) {
-                        $sortie.append($('<hr>'));
-                    }
-                    var $nouvelle = $('<p>');
+                    var $nouvelle = $('<div>').addClass('news-block');
+                    $nouvelle.append($('<div>').addClass('news-date').append(PrintDate(data.news[i].date)));
                     $nouvelle.append(data.news[i].message);
                     $sortie.append($nouvelle);
                 }
@@ -42,4 +40,11 @@ function UpdateNews() {
 
 function comp(a, b) {
     return new Date(b.date).getTime() - new Date(a.date).getTime();
+}
+
+function PrintDate(d){
+    var dt = new Date(d);
+    var dateString =  dt.getFullYear() + "/" + ("0"+(dt.getMonth()+1)).slice(-2) + "/" + ("0" + dt.getDate()).slice(-2);
+    /* dateString = dateString + " " + ("0" + dt.getHours()).slice(-2) + ":" + ("0" + dt.getMinutes()).slice(-2); */
+    return dateString;
 }
